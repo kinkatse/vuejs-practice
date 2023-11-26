0611f7c16328145ad97c32f68f8c188d202dcdbf
kakintse <kinkatse@mail.adelphi.edu>
2023-11-26 17:49:44 -0500

    Create new Vue Project

---
+++ pt2/modal-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: "My First Vue App :)"
+    }
+  }
+}
+</script>
+
+// This is global styling
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 60px;
+}
+</style>
