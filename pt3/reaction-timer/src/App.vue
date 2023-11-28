acc8437755056248bbb54629defadbadf6b487ac
kakintse <kinkatse@mail.adelphi.edu>
2023-11-27 20:25:25 -0500

    Set up game components

--- pt3/reaction-timer/src/App.vue
+++ pt3/reaction-timer/src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
     <h1>Ninja Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay"/>
   </div>
 </template>
 
 <script>
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {  }
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  }
 }
 </script>
 
